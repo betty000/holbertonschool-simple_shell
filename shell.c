2b65e6178aca261808a933f7a46e3d9c676b3086
nicolas1897 <106556299+nicolas1897@users.noreply.github.com>
2022-08-14 16:31:37 -0500

    Update shell.c

--- shell.c
+++ shell.c
@@ -1,53 +1,27 @@
-#include "main.h"
-
-
+#include "olaf.h"
 
 /**
-
  * main - Entry point of the program.
-
  * @ac: The number of parameters passed to the executable file. In the case
-
  * this variable will not be used.
-
  * @av: The name of the program.
-
  * Return: Always 0.
-
  */
-
 int main(__attribute__((unused)) int ac, char **av)
-  
 {
-  
-  char *line;
-  
-  size_t size;
-  
-  int command_counter;
-  
+	char *line;
+	size_t size;
+	int command_counter;
 
-  
-  command_counter = 0;
-  
-  signal(SIGINT, SIG_IGN);
-  
-  do {
-    
-    command_counter++;
-    
-    line = NULL;
-    
-    size = 0;
-    
-    parse_line(line, size, command_counter, av);
-    
-fflush( stdout );
-    
-  } while (1);
-  
-fflush(stdout);
-  
-  return (0);
-  
+	command_counter = 0;
+	signal(SIGINT, SIG_IGN);
+	do {
+		command_counter++;
+		line = NULL;
+		size = 0;
+		parse_line(line, size, command_counter, av);
+
+	} while (1);
+
+	return (0);
 }
