3628666460b6566400de1ac50a2cbc7d077c9e57
nicolas1897 <106556299+nicolas1897@users.noreply.github.com>
2022-08-14 16:52:39 -0500

    Update builtins.c

--- builtins.c
+++ builtins.c
@@ -45,7 +45,7 @@ void env_b(__attribute__((unused))char *line)
 	{
 		for (j = 0; environ[i][j] != '\0'; j++)
 			write(STDOUT_FILENO, &environ[i][j], 1);
-		write(STDOUT_FILENO, "\n", 1);
+		
 	}
 }
 
