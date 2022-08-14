ebea42100cf314f7b1da940551c086d13162142c
nicolas1897 <106556299+nicolas1897@users.noreply.github.com>
2022-08-14 16:32:13 -0500

    Update print_number.c

--- print_number.c
+++ print_number.c
@@ -1,59 +1,30 @@
-#include "main.h"
-
-
+#include "olaf.h"
 
 /**
-
  * print_number - Prints an unsigned number
-
  * @n: unsigned integer to be printed
-
  * Return: The amount of numbers printed
-
  */
-
 int print_number(int n)
-  
 {
-  
-  int div;
-  
-  int len;
-  
-  unsigned int num;
-  
+	int div;
+	int len;
+	unsigned int num;
 
-  
-  div = 1;
-  
-  len = 0;
-  
+	div = 1;
+	len = 0;
 
-  
-  num = n;
-  
+	num = n;
 
-  
-  for (; num / div > 9; )
-    
-    div *= 10;
-  
+	for (; num / div > 9; )
+		div *= 10;
 
-  
-  for (; div != 0; )
-    
-    {
-      
-      len += _write_char('0' + num / div);
-      
-      num %= div;
-      
-      div /= 10;
-      
-    }
-  
+	for (; div != 0; )
+	{
+		len += _write_char('0' + num / div);
+		num %= div;
+		div /= 10;
+	}
 
-  
-  return (len);
-  
+	return (len);
 }
