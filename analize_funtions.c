a50e2823a21789d8e96cd344f39f73299bcaf9f7
nicolas1897 <106556299+nicolas1897@users.noreply.github.com>
2022-08-18 14:14:43 -0500

    Update analize_funtions.c

--- analize_funtions.c
+++ analize_funtions.c
@@ -34,7 +34,7 @@ void parse_line(char *line, size_t size, int command_counter, char **av)
 			free(param_array[i]);
 		single_free(2, param_array, line); /*free array with function*/
 	}
-	else if (read_len == 0)
+	else if (read_len == 2)
 		exit_b(line); /*?*/
 	else if (read_len == 2)
 		exit_c(line);
