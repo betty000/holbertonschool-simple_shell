5e70123be6211c9b248fa36913ef5c49e4725f24
nicolas1897 <106556299+nicolas1897@users.noreply.github.com>
2022-08-18 11:33:52 -0500

    Update builtins.c

--- builtins.c
+++ builtins.c
@@ -60,6 +60,16 @@ void exit_b(char *line)
 }
 
 /**
+ * exit_c - Exits the shell. After freeing allocated resources.
+ * @line: A string representing the input from the user.
+ */
+void exit_c(char *line)
+{
+	free(line);
+	exit(EXIT_FAILURE);
+}
+
+/**
  * check_built_ins - Finds the right function needed for execution.
  * @str: The name of the function that is needed.
  * Return: Upon sucess a pointer to a void function. Otherwise NULL.
@@ -70,6 +80,7 @@ void (*check_built_ins(char *str))(char *str)
 
 	builtin_t buildin[] = {
 		{"exit", exit_b},
+		{"exit", exit_c},
 		{"env", env_b},
 		{"cd", cd_b},
 		{NULL, NULL}
