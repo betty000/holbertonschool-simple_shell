3606a13dd1db8dbfb740de23c1724cdbd3e30801
nicolas1897 <106556299+nicolas1897@users.noreply.github.com>
2022-08-18 21:26:34 -0500

    Update analize_funtions.c

--- analize_funtions.c
+++ analize_funtions.c
@@ -19,7 +19,7 @@ void parse_line(char *line, size_t size, int command_counter, char **av)
 
 	token_count = 0;
 	/*we get the lenght of the line*/
-	read_len = getline(&line, &size, stdin); 
+	read_len = getline(&line, &size, stdin);
 	if (read_len != -1) /*condition to creat array*/
 	{
 		/*array calls the function token, that separate*/
