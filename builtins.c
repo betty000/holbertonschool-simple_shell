5e3078b57e35743593069e7cc739e42246f17d10
IngMariale <98774482+IngMariale@users.noreply.github.com>
2022-08-19 01:42:43 -0500

    change places

--- builtins.c
+++ builtins.c
@@ -1,6 +1,51 @@
 #include "main.h"
 
 /**
+ * built_in - Checks for builtin functions.
+ * @command: An array of all the arguments passed to the shell.
+ * @line: A string representing the input from the user.
+ * Return: If function is found 0. Otherwise -1.
+ */
+int built_in(char **command, char *line)
+{
+	void (*build)(char *);
+
+	build = check_built_ins(command[0]);
+	if (build == NULL)
+		return (-1);
+	if (_strcmp("exit", command[0]) == 0)
+		double_free(command);
+	build(line);
+	return (0);
+}
+
+/**
+ * check_built_ins - Finds the right function needed for execution.
+ * @str: The name of the function that is needed.
+ * Return: Upon sucess a pointer to a void function. Otherwise NULL.
+ */
+void (*check_built_ins(char *str))(char *str)
+{
+	int i;
+
+	builtin_t buildin[] = {
+		{"exit", exit_b},
+		{"env", env_b},
+		{"cd", cd_b},
+		{NULL, NULL}
+	};
+
+	for (i = 0; buildin[i].built != NULL; i++)
+	{
+		if (_strcmp(str, buildin[i].built) == 0)
+		{
+			return (buildin[i].f);
+		}
+	}
+	return (NULL);
+}
+
+/**
  * cd_b - Changes the current working directory to the parameter passed to cd.
  * if no parameter is passed it will change directory to HOME.
  * @line: A string representing the input from the user.
@@ -57,49 +102,4 @@ void exit_b(char *line)
 {
 	free(line);
 	exit(0);
-}
-
-/**
- * check_built_ins - Finds the right function needed for execution.
- * @str: The name of the function that is needed.
- * Return: Upon sucess a pointer to a void function. Otherwise NULL.
- */
-void (*check_built_ins(char *str))(char *str)
-{
-	int i;
-
-	builtin_t buildin[] = {
-		{"exit", exit_b},
-		{"env", env_b},
-		{"cd", cd_b},
-		{NULL, NULL}
-	};
-
-	for (i = 0; buildin[i].built != NULL; i++)
-	{
-		if (_strcmp(str, buildin[i].built) == 0)
-		{
-			return (buildin[i].f);
-		}
-	}
-	return (NULL);
-}
-
-/**
- * built_in - Checks for builtin functions.
- * @command: An array of all the arguments passed to the shell.
- * @line: A string representing the input from the user.
- * Return: If function is found 0. Otherwise -1.
- */
-int built_in(char **command, char *line)
-{
-	void (*build)(char *);
-
-	build = check_built_ins(command[0]);
-	if (build == NULL)
-		return (-1);
-	if (_strcmp("exit", command[0]) == 0)
-		double_free(command);
-	build(line);
-	return (0);
 }
