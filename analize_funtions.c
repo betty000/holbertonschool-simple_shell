e7383d4b7dc19586b28507af0eb0eff8d0c38cd2
nicolas1897 <106556299+nicolas1897@users.noreply.github.com>
2022-08-19 09:06:55 -0500

    fixing betty white spaces

--- analize_funtions.c
+++ analize_funtions.c
@@ -35,7 +35,7 @@ void parse_line(char *line, size_t size, int command_counter, char **av)
 		for (i = 0; param_array[i] != NULL; i++)
 			free(param_array[i]);
 		single_free(2, param_array, line); /*free array with function*/
-	}	
+	}
 	else
 		exit_b(line);
 }
